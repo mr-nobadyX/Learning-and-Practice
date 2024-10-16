71f389e9a0edef60343834df43bde5c22e7ef321
mr-nobadyX <sanjaykushwaha0573@gmail.com>
2024-10-16 18:22:46 +0530

    documentations readed

--- Development/Learning/Go-lang/day-9and-10/main.go
+++ Development/Learning/Go-lang/day-9and-10/main.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Printf("fmt.Println('Namaste, creating a web Server')")
+	fmt.Println("This is actually a theory day i just read out the documentations and examples")
 }
