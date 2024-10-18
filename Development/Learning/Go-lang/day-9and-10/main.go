fcd535bffa44e557ae7588be41e2744be99314be
mr-nobadyX <sanjaykushwaha0573@gmail.com>
2024-10-18 22:25:30 +0530

    this documentation learning day

--- Development/Learning/Go-lang/day-9and-10/main.go
+++ Development/Learning/Go-lang/day-9and-10/main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Printf("fmt.Println('Namaste, creating a web Server')")
 	fmt.Println("This is actually a theory day i just read out the documentations and examples")
+	fmt.Printf("Documentation learning and about the summary of the day")
 }
