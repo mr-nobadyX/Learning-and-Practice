0139389ecfa7cfdf462538c0c49c15c423435f5f
mr-nobadyX <sanjaykushwaha0573@gmail.com>
2024-11-27 21:39:02 +0530

    Docs read

--- Development/Learning/MernStack/Backend/SmallAct/index.js
+++ Development/Learning/MernStack/Backend/SmallAct/index.js
@@ -7,7 +7,6 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-// serve the static files 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs'); 
@@ -16,7 +15,6 @@ app.get('/', (req, res)=>{
     res.render("index");
 })
 
-// dynamic routing 'basic'
 app.get('/profile:user', (req, res)=>{
     res.send(req.params.user);
     console.log("Good to go...");
