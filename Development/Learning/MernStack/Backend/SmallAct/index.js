8b22f89f5fec7c5f0c4c98bdae9f2e37082a2db2
mr-nobadyX <sanjaykushwaha0573@gmail.com>
2024-11-29 18:23:41 +0530

    More feature adding

--- Development/Learning/MernStack/Backend/SmallAct/index.js
+++ Development/Learning/MernStack/Backend/SmallAct/index.js
@@ -18,6 +18,12 @@ app.get('/', (req, res)=>{
     })  
 })
 
+app.get('/file/:filename',(req, res)=>{
+    fs.readFile(`./file/${req.params.filename}`, 'utf-8', (err, filedata)=>{
+    res.render("show");
+    })
+})
+
 app.post('/create', (req, res)=>{
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err)=>{
        res.redirect("/");
