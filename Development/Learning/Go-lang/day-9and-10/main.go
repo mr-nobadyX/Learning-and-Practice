1acaacfb4ae5b5d09a7e0118da6ebd00271340a7
mr-nobadyX <sanjaykushwaha0573@gmail.com>
2025-01-26 21:52:43 +0530

    Docs

--- Development/Learning/Go-lang/day-9and-10/main.go
+++ Development/Learning/Go-lang/day-9and-10/main.go
@@ -7,15 +7,8 @@ func main() {
 
 	// Doc reads
 	learnDoc()
-
-	// say NO to love
-	saynotoLove()
 }
 
 func learnDoc() {
 	fmt.Print("Readed Documentions")
-}
-
-func saynotoLove() {
-	fmt.Print("Love is injrious to health and life")
 }
