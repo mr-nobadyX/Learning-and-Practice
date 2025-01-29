5e69cb307d24e80949ddadedf0c20c746971d6e3
mr-nobadyX <sanjaykushwaha0573@gmail.com>
2025-01-29 22:32:59 +0530

    Docs

---
+++ Development/Learning/Go-lang/day-11/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("fmt.Println('Namaste, creating a web Server')")
+
+	// Doc reads
+	learnDoc()
+}
+
+func learnDoc() {
+	fmt.Print("Readed Documentions")
+}
