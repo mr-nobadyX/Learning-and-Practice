947144a1f83d1f27b13f1598fb54d6a341a259f8
mr-nobadyX <sanjaykushwaha0573@gmail.com>
2025-02-01 22:00:30 +0530

    Docs

--- Development/Learning/MernStack/Backend/day13Data-AsoInLargeProject/app.js
+++ Development/Learning/MernStack/Backend/day13Data-AsoInLargeProject/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 300;
+const port = 3000;
 
 const userModel = require("./models/user");
 const cookieParser = require('cookie-parser');
